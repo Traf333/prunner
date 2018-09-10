96395f1534e528d97f72b788fd5f79182ed94633
Igor Trofimov <igortr333@gmail.com>
2018-09-10 11:06:13 +1000

    write client

--- lib/client.rb
+++ lib/client.rb
@@ -1,17 +1,42 @@
 require 'httparty'
 
+# the class +Client+ is responsible for fetching data tree from the remote server
+# It has considered case for unstable servers by retrying the request.
 class Client
   include HTTParty
 
+  ExternalError = Class.new(StandardError)
+
+  DEFAULT_RETRY_COUNT = 3
+
   base_uri 'https://kf6xwyykee.execute-api.us-east-1.amazonaws.com/production'
 
-  attr_reader :options
+  attr_reader :options, :error
 
-  def initialize(options)
+  def initialize(options = {})
     @options = options
+    @error = nil
   end
 
-  def indicators
-    get('/tree/input')
+  def tree
+    tries ||= 0
+    fetch_tree
+  rescue Client::ExternalError => e
+    tries += 1
+    retry if tries < allowed_retry_count
+    @error = e.message
+    nil
+  end
+
+  private
+
+  def fetch_tree
+    self.class.get('/tree/input').tap do |response|
+      raise ExternalError.new(response.body) unless response.success?
+    end
+  end
+
+  def allowed_retry_count
+    @options.fetch(:retry, DEFAULT_RETRY_COUNT)
   end
 end
--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -2,5 +2,34 @@ require_relative '../../lib/client'
 
 RSpec.describe Client do
 
-  it { is_expected.to be }
+  describe '#tree' do
+    let(:stub_response) { double(success?: true) }
+
+    before do
+      allow(described_class).to receive(:get) { stub_response }
+    end
+
+    it { expect(subject.tree).to eq stub_response }
+
+    context 'failed request' do
+      let(:retry_count) { 2 }
+      let(:error_message) { 'Something went wrong' }
+      let(:stub_response) { double(success?: false, body: error_message) }
+      let(:successful_response) { double(success?: true) }
+
+      it 'sets an error message' do
+        expect(subject.tree).to be_nil
+        expect(subject.error).to eq error_message
+      end
+
+      it 'returns response if one of requests was successful' do
+        call_count = 0
+        allow(subject).to receive(:fetch_tree) do
+          call_count += 1
+          call_count.odd? ? raise(Client::ExternalError) : successful_response
+        end
+        expect(subject.tree).to eq successful_response
+      end
+    end
+  end
 end
